8cb885ffa9fdb753997ebd47da93226b240244fd
Emin0v <eminov-2001@inbox.ru>
2022-01-02 22:32:52 +0400

    .

---
+++ order-service/src/main/java/com/company/dto/OrderCreateDto.java
@@ -0,0 +1,12 @@
+package com.company.dto;
+
+import java.math.BigDecimal;
+
+public class OrderCreateDto {
+
+    private String productUuid;
+
+    private Integer count;
+
+    private BigDecimal price;
+}
